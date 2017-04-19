ff703bdef7166246c186243ffc3cb2b02dec08fc
davidontiveros <david.ontiveros.89@hotmail.com>
2017-04-19 01:54:51 -0500

    - integrated karma+jasmine tests - added test for HomeController

---
+++ app/gruntfile.js
@@ -0,0 +1,35 @@
+/**
+ * Created by daviD on 19/04/2017.
+ */
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        //package json containing needed Grunt plugins
+        pkg: grunt.file.readJSON('package.json'),
+
+        //small tidy up task
+        clean:{
+            start: ['javascripts/prod/*']
+        },
+
+        requirejs: {
+            build: {
+                options: {
+                    baseUrl: 'javascripts', // should be same baseUrl as the javascipts/main.js
+                    out: 'javascripts/prod/app.js', // output result file
+                    name: '../node_modules/almond/almond', // reference to almond library
+                    include: 'app', // name of our main module requirejs application (can be found @ main javascipts/main.js)
+                    mainConfigFile: 'javascripts/main.js' // path to the requirejs main file
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Default task(s)
+    grunt.registerTask('default', ['clean:start','requirejs:build']);
+};
